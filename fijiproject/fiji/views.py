1eb67de982314e417a712873f76c36115b34fc99
Real-Abdureh <abdouliabbas75@gmail.com>
2022-02-12 12:26:43 +0100

    create register form page

--- fijiproject/fiji/views.py
+++ fijiproject/fiji/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from . forms import UserRegisterForm
+from django.contrib import messages
 
 
 def home(request):
    return render(request, 'fiji/home.html')
+
+
+def register(request):
+   if request.method == "POST":
+      form = UserRegisterForm(request.POST)
+      if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         messages.success(request, f'Hi {username}, your account was created successfully')
+         return redirect('home')
+   else:
+      form = UserRegisterForm()
+
+   return render(request, 'fiji/register.html', {'form': form})
